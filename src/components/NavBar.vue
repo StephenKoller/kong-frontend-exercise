f01996cf58383750265d66378c07a54b0eff9ff3
Stephen Koller <stephenkoller@gmail.com>
2025-02-08 21:26:38 -0500

    feat(navbar.vue): Add NavBar styles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,43 @@
 <template>
   <nav>
-    <router-link to="/">
-      Home
-    </router-link>
+    <div class="left">
+      <router-link to="/">
+        <img
+          alt="Kong Konnect logo"
+          src="/public/logo-konnect.svg"
+        >
+      </router-link>
+    </div>
+
+    <div class="right">
+      <a
+        class="organization"
+        href="#"
+      >
+        <img
+          alt="people icon"
+          src="/public/icon-people.svg"
+        >
+        Organization</a>
+      <a
+        class="settings"
+        href="#"
+      >
+        <img
+          alt="gear icon"
+          src="/public/icon-cog.svg"
+        >
+        Settings</a>
+      <a
+        class="userMenu"
+        href="#"
+      >
+        <img
+          alt="person icon"
+          src="/public/icon-avatar.svg"
+        >
+        Katherine Ellis</a>
+    </div>
   </nav>
 </template>
 
@@ -10,17 +45,59 @@
 
 <style lang="scss" scoped>
 nav {
+    align-items: center;
+
+    background: linear-gradient(180deg, #09224F 0%, #073382 100%);
     border-bottom: 1px solid #333;
-    margin-bottom: 4rem;
-    padding: 16px;
+
+    display: flex;
+    flex-direction: row;
+    height: 70px;
+    justify-content: space-between;
+
+}
+
+.right {
+
+    display: flex;
+    flex-direction: row;
+    /* need this set to 100% to allow child to take up full height for bg color */
+    height: 100%;
+    justify-content: space-between;
 
     a {
-        color: blue;
-        font-weight: 700;
+        align-items: center;
+        color: #FFFFFF;
+        display: flex;
+        font-size: 15px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 18px;
+        margin: 0 16px;
+        opacity: 0.8;
+        text-decoration: none;
 
-        &.router-link-exact-active {
-            color: green;
+        &:hover {
+            opacity: 1;
+        }
+
+        &:last-of-type {
+            margin-right: 0;
         }
     }
+
+    img {
+        margin-right: 12px;
+    }
+
+
+    .userMenu {
+        background-color: #072863;
+        height: 100%;
+        padding: 0 20px;
+    }
 }
+
+
+
 </style>
