2b92d5a5f699a67becd9d80a8202b978b5d2a05c
Stephen Koller <stephenkoller@gmail.com>
2025-02-11 23:44:47 -0500

    chore: Add missing button files

---
+++ src/components/BackArrowButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    :disabled="paginationStore.currentPage === 0"
+    @click="paginationStore.setPage(paginationStore.currentPage - 1)"
+  >
+    <svg
+      fill="none"
+      height="44"
+      viewBox="0 0 44 44"
+      width="44"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="22"
+        cy="22"
+        r="21.5"
+        :stroke="activeColor"
+      />
+      <path
+        d="M14.8457 22.4961L18.6426 18.6992V21.8457H28.9434V23.1465H18.6426V26.293L14.8457 22.4961Z"
+        :fill="activeColor"
+        fill-opacity="0.25"
+      />
+    </svg>
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { ref, defineProps } from 'vue'
+import { usePaginationStore } from '@/stores/pagination'
+
+const paginationStore = usePaginationStore()
+
+const props = defineProps<{
+  disabled: boolean
+}>()
+
+const activeColor = ref<string>('#A6C6FF')
+
+if (props.disabled) {
+  activeColor.value = 'black'
+} else {
+  activeColor.value = '#A6C6FF'
+}
+
+</script>
---
+++ src/components/ForwardArrowButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <button
+    :disabled="disabled"
+    @click="paginationStore.setPage(paginationStore.currentPage + 1)"
+  >
+    <svg
+      fill="none"
+      height="44"
+      viewBox="0 0 44 44"
+      width="44"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="22"
+        cy="22"
+        r="21.5"
+        :stroke="activeColor"
+      />
+      <path
+        d="M29.1543 22.4961L25.3574 18.6992V21.8457H15.0566V23.1465H25.3574V26.293L29.1543 22.4961Z"
+        :fill="activeColor"
+      />
+    </svg>
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { ref, defineProps } from 'vue'
+import { usePaginationStore } from '@/stores/pagination'
+
+const paginationStore = usePaginationStore()
+
+const props = defineProps<{
+  disabled: boolean
+}>()
+
+const activeColor = ref<string>('#A6C6FF')
+
+if (props.disabled) {
+  activeColor.value = 'black'
+} else {
+  activeColor.value = '#A6C6FF'
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  background-color: transparent;
+  border: none;
+}
+</style>
